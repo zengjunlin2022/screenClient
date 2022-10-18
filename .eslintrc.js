5c0d84a2d593ba570bb74496428d7aa68f14128a
zengjunlin2022 <ken.zeng@sharpasia.com.mo>
2022-10-18 09:34:06 +0800

    init2

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'extends': 'plugin:vue/recommended',
-    parserOptions: {
-        parser: 'babel-eslint',
-        sourceType: 'module',
-        allowImportExportEverywhere: true
-    },
-    rules: undefined
-}
